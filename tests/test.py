1b382b1996c91152fae8bad43bf124f5c6a30eb9
Daniel Hajjar <daniel.hajjar@learnet.se>
2020-08-31 15:59:03 +0200

    Kommentarer och små förbättringar

--- tests/test.py
+++ tests/test.py
@@ -4,30 +4,27 @@ import os
 
 driver = webdriver.Chrome()
 
-
 # Testar om hemsidan är tillgänglig från internet
 try:
     driver.get("https://fabilus.gitlab.io/frisorhemsida/")
 except:
     print("Hemsida inte tillgänglig på internet")
 
+# Upplösningar som programmet ska ta screenshots av
+resolutions = [[1024, 768],
+               [1280, 800],
+               [1366, 768],
+               [1920, 1080],
+               [2560, 1440]]
 
-# Tar screenshots på hemsidan i olika upplösningar
-resolutions = [[1024, 768], [1280, 800], [
-    1366, 768], [1920, 1080], [2560, 1440]]
-
+# Kollar om det finns en mapp för screenshots och skapar mappen ifall den inte existerar
 if not os.path.exists("screenshots"):
     os.mkdir("screenshots")
 
-for (w, h) in resolutions:
+# Ta screenshots i alla specifierade upplösningar och spara resultaten som "BreddxHöjd.png" i screenshots
+for (width, height) in resolutions:
+    driver.set_window_size(width, height)
+    driver.save_screenshot("screenshots/{}x{}.png".format(width, height))
 
-    driver.set_window_size(w, h)
-    driver.save_screenshot("screenshots/{}x{}.png".format(w, h))
-
+# Avsluta programmet
 driver.quit()
-
-# driver.set_window_size(1920, 1080)
-# driver.save_screenshot('screentest.png')
-
-# driver.set_window_size(1120, 1080)
-# driver.save_screenshot('screentest2.png')
