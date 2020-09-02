5c9787ab92857c7ca12c1723aa80751122b578c5
Joakim Kokocha <joakim.kokocha@learnet.se>
2020-09-02 15:52:11 +0200

    Bytte output-typ i validation.bat till gnu

--- tests/validation.bat
+++ tests/validation.bat
@@ -1,4 +1,4 @@
-curl.exe -F output=text -F "text=<../style.css" https://jigsaw.w3.org/css-validator/validator
-curl.exe -F out=text -F "content=<../index.html" https://validator.nu/
+curl.exe -F output=gnu -F "text=<../style.css" https://jigsaw.w3.org/css-validator/validator
+curl.exe -F out=gnu -F "content=<../index.html" https://validator.nu/
 
 pause
