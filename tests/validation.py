06de937bb9728ed3bde4194acaf3e3439455cf43
Joakim Kokocha <joakim.kokocha@learnet.se>
2020-09-02 11:27:24 +0200

    Började skriva om validation.py

--- tests/validation.py
+++ tests/validation.py
@@ -1,23 +1,22 @@
 import re
-import urllib.request as request
-import json
+import requests
+validatorURL = "http://validator.nu/?out=json""&""doc="
+
+# def validate(URL):
+#      + \
+#         request.quote(URL)
+#     print(validatorURL)
+#     req = request.Request(validatorURL)
+#     req.get_method = lambda: "HEAD"
+#     response = request.urlopen(req)
+#     print(response.read())
+#     valid = (response.getheader("X-W3C-Validator-Status") == "Valid")
+
+#     errors = int(response.getheader("X-W3C-Validator-Errors"))
+#     warnings = int(response.getheader("X-W3C-Validator-Warnings"))
+
+#     print("Valid markup: {} (Errors: {}, Warnings: {}".format(
+#         valid, errors, warnings))
 
 
-def validate(URL):
-    validatorURL = "http://validator.nu/?out=json""&""doc=" + \
-        request.quote(URL)
-    print(validatorURL)
-    req = request.Request(validatorURL)
-    req.get_method = lambda: "HEAD"
-    response = request.urlopen(req)
-    print(response.read())
-    valid = (response.getheader("X-W3C-Validator-Status") == "Valid")
-
-    errors = int(response.getheader("X-W3C-Validator-Errors"))
-    warnings = int(response.getheader("X-W3C-Validator-Warnings"))
-
-    print("Valid markup: {} (Errors: {}, Warnings: {}".format(
-        valid, errors, warnings))
-
-
-validate("http://fabilus.glab.io/frisorhemsida")
+# validate("http://fabilus.glab.io/frisorhemsida")
