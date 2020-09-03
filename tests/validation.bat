c4e770b5cfbd5ac6b9ca85760c3c777cf6c27181
Joakim Kokocha <joakim.kokocha@learnet.se>
2020-09-03 12:02:31 +0200

    ändrade platsen på style.css

--- tests/validation.bat
+++ tests/validation.bat
@@ -1,6 +1,6 @@
 @echo off
 
-curl.exe -F output=gnu -F "text=<../style.css" https://jigsaw.w3.org/css-validator/validator
+curl.exe -F output=gnu -F "text=<../css/style.css" https://jigsaw.w3.org/css-validator/validator
 curl.exe -F asciiquotes=yes -F out=gnu -F "content=<../index.html" https://validator.nu/
 
 pause
