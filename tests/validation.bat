f57674b8e08ba70eca3ab05336af3e307e86db63
Joakim Kokocha <joakim.kokocha@learnet.se>
2020-09-03 12:00:53 +0200

    lade till @echo off

--- tests/validation.bat
+++ tests/validation.bat
@@ -1,3 +1,5 @@
+@echo off
+
 curl.exe -F output=gnu -F "text=<../style.css" https://jigsaw.w3.org/css-validator/validator
 curl.exe -F asciiquotes=yes -F out=gnu -F "content=<../index.html" https://validator.nu/
 
