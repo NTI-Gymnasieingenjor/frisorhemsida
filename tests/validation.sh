20eb688d2cd5c55700125e0fbc168d48306eff72
Joakim Kokocha <joakim.kokocha@learnet.se>
2020-09-07 11:18:16 +0200

    ändrade path i validation.sh för style.css

--- tests/validation.sh
+++ tests/validation.sh
@@ -1,2 +1,2 @@
-curl -F output=gnu -F "text=<../style.css" https://jigsaw.w3.org/css-validator/validator
+curl -F output=gnu -F "text=<../css/style.css" https://jigsaw.w3.org/css-validator/validator
 curl -F out=gnu -F "content=<../index.html" https://validator.nu/
