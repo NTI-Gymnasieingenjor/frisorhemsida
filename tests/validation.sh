427967c79a92214e77885d870ed19b73565afcb0
Joakim Kokocha <joakim.kokocha@learnet.se>
2020-09-07 14:27:35 +0200

    Kommenterade validation.sh

--- tests/validation.sh
+++ tests/validation.sh
@@ -1,2 +1,4 @@
+# output=gnu ger fin output, med en rad per error
+
 curl -F output=gnu -F "text=<../css/style.css" https://jigsaw.w3.org/css-validator/validator
 curl -F out=gnu -F "content=<../index.html" https://validator.nu/
