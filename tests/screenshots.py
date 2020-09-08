95b73844f181dfe2dcca55ba7bfb8c7a7f0434b5
Daniel Hajjar <daniel.hajjar@learnet.se>
2020-09-08 14:03:36 +0200

    Kommentarer, fixade pathen och errorhantering.

--- tests/screenshots.py
+++ tests/screenshots.py
@@ -2,28 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 import sys
+import requests
 
-# Startar chrome "headless", alltså att webbläsar-rutan inte syns
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-
-driver = webdriver.Chrome(options=chrome_options)
-
-# Kollar om ett argument har skickats med, i vilket fall hemsidan som ligger uppe testas istället för filerna på datorn
-# Starta filen i VSCode med F5 (debugging) för att skicka med argumentet "online", använd annars [CTRL + F5] (normal)
-if len(sys.argv) > 1:
-    page = "https://fabilus.gitlab.io/frisorhemsida"
-else:
-    page = "file://{}\index.html".format(os.getcwd())
-
-# Testar om filen existerar/hemsidan ligger uppe
-try:
-    driver.get(page)
-except:
-    print("Kan inte hitta filen/länken")
-    quit()
-
-# Upplösningar som programmet ska ta screenshots av
+# Upplösningar som selenium ska ta screenshots av
 resolutions = [[1024, 768],
                [1280, 800],
                [1366, 768],
@@ -31,14 +12,46 @@ resolutions = [[1024, 768],
                [2560, 1440],
                [3840, 2160]]
 
-# Skapar en mapp för screenshots ifall den inte existerar
-if not os.path.exists("screenshots"):
-    os.mkdir("screenshots")
+# Startar chrome "headless", alltså att webbläsar-rutan inte syns
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
-# Ta screenshots i alla specifierade upplösningar och spara resultaten som "BreddxHöjd.png" i screenshots
-for (width, height) in resolutions:
-    driver.set_window_size(width, height)
-    driver.save_screenshot("screenshots/{}x{}.png".format(width, height))
+driver = webdriver.Chrome(options=chrome_options)
 
-# Avsluta programmet
+try:
+    # Kollar om argumentet "online" skickades med, i vilket fall hemsidan på nätet testas. Annars testas den lokala filen.
+    if len(sys.argv) > 1 and sys.argv[1] == "online":
+        page = "https://fabilus.gitlab.io/frisorhemsida"
+
+        req = requests.get(page)
+        req.close()
+
+        # Kollar om hemsidan inte går att hitta online, ändrar strängen Exception om detta är fallet
+        if req.status_code != requests.codes['ok']:
+            raise Exception("Statuskod: " + str(req.status_code) +
+                            "\nLänk: " + page)
+
+    else:
+        # Kollar om filen existerar
+        page = "{}/public/index.html".format(os.getcwd())
+        if not os.path.exists(page):
+            raise Exception("Kan inte hitta filen: " + page)
+
+    driver.get(page)
+
+    # Skapar en mapp för screenshots ifall den inte existerar
+    if not os.path.exists("screenshots"):
+        os.mkdir("screenshots")
+
+    # Tar screenshots i alla specifierade upplösningar och spara resultaten som "BreddxHöjd.png" i screenshots
+    for (width, height) in resolutions:
+        driver.set_window_size(width, height)
+        driver.save_screenshot("screenshots/{}x{}.png".format(width, height))
+
+# Skriver ut error-meddelanden ifall de uppstår
+except Exception as err:
+    print("ERROR:\n" + str(err), file=sys.stderr)
+
+
+# Avslutar chrome
 driver.quit()
--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 import sys
+import requests
 
 # Startar chrome "headless", alltså att webbläsar-rutan inte syns
 chrome_options = Options()
@@ -9,23 +10,36 @@ chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome(options=chrome_options)
 
-# Kollar om ett argument har skickats med, i vilket fall hemsidan som ligger uppe testas istället för filerna på datorn
-# Starta filen i VSCode med F5 (debugging) för att skicka med argumentet "online", använd annars [CTRL + F5] (normal)
-if len(sys.argv) > 1:
-    page = "https://fabilus.gitlab.io/frisorhemsida"
-else:
-    page = "file://{}\index.html".format(os.getcwd())
-
-# Testar om filen existerar/hemsidan ligger uppe
 try:
+    # Kollar om ett argumenar skickats med, i vilket fall hemsidan som ligger uppe testas istället för filerna på datorn
+    # Starta filen i VSCode F5(debugging) för att skicka med argumentet "online", använd annars[CTRL + F5](normal)
+    if len(sys.argv) > 1 and sys.argv[1] == "online":
+        page = "https://fabilus.gitlab.io/frisorhemsida"
+
+        req = requests.get(page)
+        req.close()
+
+        # Kollar om hemsidan går att komma åt utan några problem
+        if req.status_code != requests.codes['ok']:
+            raise Exception("Statuskod: " + str(req.status_code) +
+                            "\nLänk: " + page)
+
+    else:
+        # Kollar om filen existerar
+        page = "{}/public/index.html".format(os.getcwd())
+        if not os.path.exists(page):
+            raise Exception("Kan inte hitta filen: " + page)
+
     driver.get(page)
-except:
-    print("Kan inte hitta filen/länken")
-    quit()
 
-# Kollar om företagets namn står någonstans på sidan
-if "Salong Gloria" not in driver.page_source:
-    print("Det finns inget meddelande om att sidan tillhör företaget!")
+    # Kollar om företagets namn står någonstans på sidan
+    if "Salong Gloria" not in driver.page_source:
+        raise Exception(
+            "Det finns inget meddelande om att sidan tillhör företaget!")
 
-# Avsluta programmet
+# Skriver ut error-meddelanden ifall de uppstår
+except Exception as err:
+    print("ERROR:\n" + str(err), file=sys.stderr)
+
+# Avslutar chrome
 driver.quit()
