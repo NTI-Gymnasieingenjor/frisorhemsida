0e1eb8a3bf23880aaf69c5222ebd67f2520d91a9
Daniel Hajjar <daniel.hajjar@learnet.se>
2020-09-08 14:02:38 +0200

    Ändrade till rätt path.

--- tests/validation.sh
+++ tests/validation.sh
@@ -1,8 +1,8 @@
 # Sparar alla errors till variabler. output=gnu ger fin output, med en rad per error
 
-cssout=$(curl -s -F output=gnu -F "text=<../css/style.css" https://jigsaw.w3.org/css-validator/validator)
+cssout=$(curl -s -F output=gnu -F "text=<../public/css/style.css" https://jigsaw.w3.org/css-validator/validator)
 
-htmlout=$(curl -s -F out=gnu -F "content=<../index.html" https://validator.nu/)
+htmlout=$(curl -s -F out=gnu -F "content=<../public/index.html" https://validator.nu/)
 
 
 # Kollar om HTML-validatorn ger några errors, och printar dem isåfall (echo). Annars printas det att allt ser bra ut.
