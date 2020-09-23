069f9d61564ec6eeaea8878463ad536d2d917c13
Fredrik Nyberg <fredriknyberg1@gmail.com>
2020-09-23 15:30:43 +0200

    Added some comments

--- tests/python_html_validering.py
+++ tests/python_html_validering.py
@@ -12,19 +12,25 @@ options.headless = True
 
 # Two variables with diffrent file paths
 PATH = "C:\Program Files (x86)\chromedriver.exe"
+# NOTE : Switch out your user to your own, do not use "fredrik.nyberg" as a file path on your PC
+#        Make sure it's in the correct path
 PATH2 = "C:\\Users\\fredrik.nyberg\\Documents\\GitHub\\frisorhemsida\\public\\index.html"
 
 # Variable with location to chromedriver.exe and the headless option
 driver = webdriver.Chrome(PATH, options=options)
 
+# ==========================================================================================================
+
 # Validates Locally
+
+# ==========================================================================================================
 
 # Loads in the website
 driver.get("https://html5.validator.nu/")
 
-# Variable which looks in dropdown menu "docselect"
+# Variable which looks in dropdown menu element "docselect"
 select = Select(driver.find_element_by_id("docselect"))
-# Looks for "File Upload" in the visable dropdown menu "docselect"
+# Looks for element "File Upload" in the visable dropdown menu "docselect"
 select.select_by_visible_text("File Upload")
 
 # Looks for the id "doc" element and sends the file path to index aka PATH2
@@ -47,7 +53,11 @@ except:
     for mistakes in driver.find_elements_by_tag_name('ol'):
        print(mistakes.text, "\n")
 
+# ==========================================================================================================
+
 # Validates Online
+
+# ==========================================================================================================
 
 driver.get("https://html5.validator.nu/")
 
