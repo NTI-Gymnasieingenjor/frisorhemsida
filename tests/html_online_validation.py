c2b56dbb2daf88acbb89e675bdf59f9415584d3e
SebastianLanden <sebastian.landen@elev.ga.ntig.se>
2020-09-24 11:56:22 +0200

    Revert "Update html_online_validation.py"
    This reverts commit 06fb4fb6742b2436e7c73a66e1107858afede934.

--- tests/html_online_validation.py
+++ tests/html_online_validation.py
@@ -1,6 +1,5 @@
 # Imports the necessary selenium extensions
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
@@ -13,8 +12,6 @@ options.headless = True
 
 # Variable with location to chromedriver.exe and the headless option
 driver = webdriver.Chrome(options=options)
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 # Validates Online
