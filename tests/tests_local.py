c486bf397ba8556859d195e7afe070a20bf31887
Klado555 <klaudiusz.pawezka@elev.ntig.se>
2020-09-24 15:30:45 +0200

    added tests for everything on the page

--- tests/tests_local.py
+++ tests/tests_local.py
@@ -20,8 +20,29 @@ desiredPath = "\\public\\index.html"
 # Loads in the website
 driver.get(parentPath+desiredPath)
 
+# Variable which will look for the specified text by id
+element = driver.find_element_by_id('title')
+
 # Variable which will look for the specified text ("//*[contains(text(), 'Desired text here')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'Tjänster')]")
+element = driver.find_element_by_id('table-services')
+
+element = driver.find_element_by_id('smallImages')
+element = driver.find_element_by_id('longImage')
+
+
+element = driver.find_element_by_xpath("//*[contains(text(), 'Kontaktinfo')]")
+element = driver.find_element_by_xpath("//*[contains(text(), '0630-555-555')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'info@fabilus.gitlab.io')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'Fjällgatan 32H')]")
+
 element = driver.find_element_by_xpath("//*[contains(text(), 'Öppettider')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'Mån‑Fre:')]")
+element = driver.find_element_by_xpath("//*[contains(text(), '10‑16')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'Lördag:')]")
+element = driver.find_element_by_xpath("//*[contains(text(), '12‑15')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'Söndag:')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'Stängt')]")
 
 # Prints out the element that wasn't found on the website
 print(element) 
