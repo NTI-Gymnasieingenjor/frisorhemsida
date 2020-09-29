eb07ef13f45d16b4c5f6e2ca1a01d84caed0a4c1
Klado555 <klaudiusz.pawezka@elev.ntig.se>
2020-09-29 15:25:50 +0200

    website tests created

--- tests/website_tests.py
+++ tests/website_tests.py
@@ -24,19 +24,16 @@ driver.get("file:///" + parentPath + desiredPath)
 
 # Variable which will look for the specified text by id
 element = driver.find_element_by_id('title')
+element = driver.find_element_by_id('titleBackground')
+element = driver.find_element_by_id('bookBtn')
 
 # Variable which will look for the specified text ("//*[contains(text(), 'Desired text here')]")
 element = driver.find_element_by_xpath("//*[contains(text(), 'Tjänster')]")
-element = driver.find_element_by_id('tableServices')
+element = driver.find_element_by_id('services')
+element = driver.find_element_by_id('smallImage1')
 
-element = driver.find_element_by_id('smallImages')
+element = driver.find_element_by_id('smallImage2')
 element = driver.find_element_by_id('longImage')
-
-
-element = driver.find_element_by_xpath("//*[contains(text(), 'Kontaktinfo')]")
-element = driver.find_element_by_xpath("//*[contains(text(), '0630-555-555')]")
-element = driver.find_element_by_xpath("//*[contains(text(), 'info@fabilus.gitlab.io')]")
-element = driver.find_element_by_xpath("//*[contains(text(), 'Fjällgatan 32H')]")
 
 element = driver.find_element_by_xpath("//*[contains(text(), 'Öppettider')]")
 element = driver.find_element_by_xpath("//*[contains(text(), 'Mån‑Fre:')]")
@@ -45,6 +42,11 @@ element = driver.find_element_by_xpath("//*[contains(text(), 'Lördag:')]")
 element = driver.find_element_by_xpath("//*[contains(text(), '12‑15')]")
 element = driver.find_element_by_xpath("//*[contains(text(), 'Söndag:')]")
 element = driver.find_element_by_xpath("//*[contains(text(), 'Stängt')]")
+
+element = driver.find_element_by_xpath("//*[contains(text(), 'Kontaktinfo')]")
+element = driver.find_element_by_xpath("//*[contains(text(), '0630-555-555')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'info@fabilus.gitlab.io')]")
+element = driver.find_element_by_xpath("//*[contains(text(), 'Fjällgatan 32H')]")
 
 # Prints out the element that wasn't found on the website
 print(element)
