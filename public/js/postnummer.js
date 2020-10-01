ed79083b82deea44dcfcf48b4da63a8147f94763
Fredrik Nyberg <fredriknyberg1@gmail.com>
2020-10-01 15:30:38 +0200

    Added Zipcode script
    ft. popover

--- public/js/postnummer.js
+++ public/js/postnummer.js
@@ -1,72 +1,52 @@
-document.getElementById("submit-zip").addEventListener("click", zipCheck)
 
-let workingZips = ["98138", "98139", "98140", "98142"]
-let submitZip = document.getElementById("submit-zip")
+// Make a list with the compatible zipcodes
+const listOfZipCodes = [
+    "98138",
+    "98139",
+    "98140",
+    "98142"
+];
 
+// Makes variables
+let zipInputElement;
+let zipInputText;
+let inputSubmitButton;
+
+// Defines variables
+zipInputElement = document.getElementById("input-zipcode")
+inputSubmitButton = document.getElementById("submit-zip")
+
+// When button is pressed, run zipCheck function
+inputSubmitButton.addEventListener("click", zipCheck)
+
+// Prevents site from reloading when pressing enter in form
+$("#form-zip").keypress(function(e) {
+    //Enter key
+    if (e.which == 13) {
+        e.preventDefault();
+        zipCheck()
+    }
+  });
+
+// Looks through list of compatible zipcodes to see if they match
+// If they match, creates a popover == > "Vi kör hem till dig!"
+// If not, creates a popover == > "Vi kör inte hem till dig!"
+// NOTE: Popover aka the little box that appears under the element
 function zipCheck() {
-    let inputValue = textInputElement.value.replace(" ", "")
-}
-
-onload = (() => {
-        let input = document.getElementById("input-zipcode")
-        let submitZip = document.getElementById("submit-zip")
-
-        input.addEventListener("keyup", function (event) {
-            if (e.key === "Enter") {
-                e.preventDefault();
-                submitZip.click();
-        };
-        
-    });
-});
-
-// let workingZips = ["98138", "98139", "98140", "98142"]
-
-// function zipCheck(){
-
-//     let input = document.getElementById("input-zipcode")
-//     // Startvärde på variabel; Hur länge loopen körs; vad som händer när loopen körts
-//     for (i = 0; i < 4; i++) {
-//         if (input == workingZips[i]) {
-//             document.getElementById("result").innerHTML = "Vi kör hem till dig!";
-//             break;
-//         }
-//         else {
-//             document.getElementById("result").innerHTML = "Vi kör inte hem till dig!"; 
-//         }
-//     }   
-// }
-
-// let workingZips = ["98138", "98139", "98140", "98142"]
-// let input = document.getElementById("input-zipcode").value
-// let currentZip = workingZips[input]
-
-// function zipCheck() {
-//     let input = document.getElementById("input-zipcode").value
-
-//     return input
-// }
-
-// function zipCheck() {
-//     let workingZips = ["98138", "98139", "98140", "98142"]
-//     let input = document.getElementById("input-zipcode").value
-//     let currentZip = workingZips[input]
-//     switch (currentZip) {
-//         case "98138": 
-//             document.getElementById("result").innerHTML = "Vi kör hem till dig!";
-//             break;
-//         case "98139": 
-//             document.getElementById("result").innerHTML = "Vi kör hem till dig!";
-//             break;
-//         case "98140": 
-//             document.getElementById("result").innerHTML = "Vi kör hem till dig!";
-//             break;
-//         case "98142": 
-//             document.getElementById("result").innerHTML = "Vi kör hem till dig!";
-//             break;
-//         default:
-//             document.getElementById("result").innerHTML = "Vi kör inte hem till dig!";
-//             break;
-//         return input
-//     }
-// }
+    zipInputText = zipInputElement.value.replace(/\s/g,'')  
+    for (i = 0; i < 4; i++) {
+        if (zipInputText == listOfZipCodes[i]) {
+            $("#input-zipcode").attr("data-content", "Vi kör hem till dig!")
+            $("#input-zipcode").popover('enable')
+            $("#input-zipcode").popover('show')
+            $("#input-zipcode").popover('disable')
+            break;
+        } 
+        else {
+            $("#input-zipcode").attr("data-content", "Vi kör inte hem till dig!")
+            $("#input-zipcode").popover('enable')
+            $("#input-zipcode").popover('show')
+            $("#input-zipcode").popover('disable')
+        }
+    }
+}   
